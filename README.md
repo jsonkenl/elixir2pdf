82b43d7d5124b59c14bd07a2576682f778c35025
Jason Kennell <kennellroxco@gmail.com>
2016-09-21 12:25:46 -0600

    Began README.md and core functions

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # Elixir2pdf
 
-**TODO: Add description**
+Elixir2pdf is a simple Elixir wrapper for ErlGuten, a Erlang PDF generation application. It is limited to my current needs of ErlGuten's functionality as well as some additional functionality not provided by ErlGuten, but I will continue to explore ErlGuten and improve Elixir2pdf over time. If you require specific functionality of ErlGuten that is not currently provided, please submit an [issue](https://github.com/kennellroxco/elixir2pdf/issues). 
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+The package can be installed via GitHub:
 
-  1. Add elixir2pdf to your list of dependencies in `mix.exs`:
+    def deps do
+      [{:elixir2pdf, github: "kennellroxco/elixir2pdf"}]
+    end
 
-        def deps do
-          [{:elixir2pdf, "~> 0.0.1"}]
-        end
+## Basic Usage
 
-  2. Ensure elixir2pdf is started before your application:
+## Contributing 
 
-        def application do
-          [applications: [:elixir2pdf]]
-        end
+Contributions are encouraged. Feel free to fork the repo, add your code along with appropriate tests and documentation (ensuring all existing tests continue to pass) and submit a pull request.
+
 
--- lib/elixir2pdf.ex
+++ lib/elixir2pdf.ex
@@ -1,2 +1,118 @@
 defmodule Elixir2pdf do
+  @moduledoc """
+  Elixir2pdf is a simple Elixir wrapper for ErlGuten, a Erlang PDF generation application. 
+  See README.md for a general overview.
+  """
+
+  @doc """
+  Starts a new PDF process and returns a process identifier (pid). 
+  """
+  def new do
+    :eg_pdf.new
+  end
+
+  @doc """
+  Creates a new page and returns a process identifier (pid). Any subsequent activity will occur
+  on the new page. Note: Elixir2pdf.new/0 takes care of this, so it is not necessary for the 
+  first page.
+
+  ## Parameters
+  - `pid` - the process identifier of the current PDF process.
+  """
+  def set_new_page(pid) do
+    page_no = pid |> :eg_pdf.new_page
+    pid |> :eg_pdf.set_page(page_no)
+    pid
+  end
+
+  @doc """
+  Sets the font that will apply to all text that follows. 
+
+  ## Parameters
+  - `pid` - the process identifier of the current PDF process.
+  - `name` - name of the font in `string` format. See the FONTS documentation for a list of 
+  available fonts.
+  - `size` - font size in `integer` format.
+  - `color` - name of font color in `atom` format. See the COLORS documentation for a list of
+  available colors. Default is `:black`.
+  """
+  def set_font(pid, name, size, color \\ :black) do
+    pid |> :eg_pdf.set_fill_color(color)
+    pid |> :eg_pdf.set_font(name |> String.to_charlist, size)
+    pid
+  end
+
+  @doc """
+  Adds text to a PDF process at a given coordinate. See the PDF_COORDINATES documentation for 
+  additional detail on how PDF document navigation works.
+
+  ## Parameters
+  - `pid` - the process identifier of the current PDF process.
+  - `x` - the horizontal coordinate point in `integer` format.
+  - `y` - the vertical coordinate point in `integer` format.
+  - `text` - the actual text to be added in `string` format.
+  """
+  def text(pid, x, y, text) do
+    pid |> :eg_pdf.begin_text
+    pid |> :eg_pdf.set_text_pos(x, y)
+    pid |> :eg_pdf.text(text |> String.to_charlist)
+    pid |> :eg_pdf.end_text
+    pid
+  end
+
+  @doc """
+  Adds text to a PDF process at a given coordinate with right-hand justification.
+
+  ## Parameters
+  - `pid` - the process identifier of the current PDF process.
+  - `set_font` - a tuple containing the name and font size of the current font set
+  (i.e. {"Helvetica", 8}).
+  - `x` - the horizontal coordinate point where the text will end in `integer` format.
+  - `y` - the vertical coordinate point in `integer` format.
+  - `text` - the actual text to be added in `string` format.
+  """
+  def right_aligned_text(pid, set_font, x, y, text) do
+    {font, size} = set_font
+    text   = text |> String.to_charlist
+    font   = font |> String.to_charlist
+    length = pid  |> :eg_pdf.get_string_width(font, size, text)
+    x      = x - length
+
+    pid |> :eg_pdf.begin_text
+    pid |> :eg_pdf.set_text_pos(x, y)
+    pid |> :eg_pdf.text(text)
+    pid |> :eg_pdf.end_text
+    pid
+  end
+
+  @doc """
+  Adds text to a PDF process that is centered between the two horizontal points given.
+
+  ## Parameters
+  - `pid` - the process identifier of the current PDF process.
+  - `x1` - the starting horizontal coordinate point in `integer` format.
+  - `x2` - the ending horizontal coordinate point in `integer` format.
+  - `y` - the vertical coordinate point in `integer` format.
+  - `set_font` - a tuple containing the name and font size of the current font set
+  (i.e. {"Helvetica", 8}).
+  - `text` - the actual text to be added in `string` format.
+  """
+  def center_aligned_text(pid, x1, x2, y, set_font, text) do
+    {font, size} = set_font
+    text   = text |> String.to_charlist
+    font   = font |> String.to_charlist
+    length = pid  |> :eg_pdf.get_string_width(font, size, text)
+    start  = (x2 - x1) 
+             |> Kernel.-(length) 
+             |> Kernel./(2) 
+             |> Kernel.+(x1) 
+             |> round
+
+    pid |> :eg_pdf.begin_text
+    pid |> :eg_pdf.set_text_pos(start, y)
+    pid |> :eg_pdf.text(text)
+    pid |> :eg_pdf.end_text
+    pid
+  end
+
 end
